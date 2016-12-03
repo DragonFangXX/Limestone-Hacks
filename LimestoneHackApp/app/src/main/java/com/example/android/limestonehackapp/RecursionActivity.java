ab5758368e01f93abe9e9f2ee028ededb31207c3
shiro-nk <hiroshikerr@gmail.com>
2016-12-03 09:13:09 -0500

    FixedHanoi

--- LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/RecursionActivity.java
+++ LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/RecursionActivity.java
@@ -11,64 +11,88 @@ public class RecursionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recursion);
     }
 
-    protected class Hanoi {
+    public class Hanoi {
         private HanoiTower[] towers = new HanoiTower[3];
 
         public Hanoi(int discs) {
-            for(HanoiTower t : towers) {
-                t = new HanoiTower(discs);
+            for(int i = 1; i<3; i++) {
+                towers[i] = new HanoiTower(discs);
             }
 
-            HanoiBlock[] blocks = new HanoiBlock[4];
+            HanoiBlock[] blocks = new HanoiBlock[discs];
             for(int i = 0; i<discs; i++)
-                blocks[i] = new HanoiBlock(discs-i);
+                blocks[i] = new HanoiBlock(i+1);
 
             towers[0] = new HanoiTower(blocks);
+
+
         }
 
         public void solve(int discs, HanoiTower src, HanoiTower tgt, HanoiTower aux) {
             if(discs>0) {
                 solve(discs-1, src, aux, tgt);
                 src.moveTopTo(tgt);
-                solve(discs-1, aux, src, tgt);
+                for(HanoiTower t : towers) t.print();
+                solve(discs-1, aux, tgt, src);
             }
         }
-    }
 
-    protected class HanoiBlock {
-
-        private int size = 0;
-
-        public HanoiBlock(int size) {
-            this.size = size;
+        public HanoiTower getTower(int i) {
+            if(0 <= i && i < towers.length)
+                return towers[i];
+            else
+                return null;
         }
 
-        public int getSize() {
-            return size;
-        }
-    }
+        protected class HanoiBlock {
 
-    protected class HanoiTower {
+            private int size = 0;
 
-        // Higher index is top
-        private HanoiBlock[] blocks;
-        private int top = 0;
+            public HanoiBlock(int size) {
+                this.size = size;
+            }
 
-        public HanoiTower(HanoiBlock[] blocks) {
-            this.blocks = blocks;
+            public int getSize() {
+                return size;
+            }
         }
 
-        public HanoiTower(int numblocks) {
-            blocks = new HanoiBlock[numblocks];
-        }
+        protected class HanoiTower {
 
-        public void moveTopTo(HanoiTower t) {
-            t.addToTop(blocks[top]);
-            blocks[top--] = null;
-        }
+            // Higher index is top
+            private HanoiBlock[] blocks;
+            private int top = 0;
 
-        protected void addToTop(HanoiBlock b) {
-            blocks[++top] = b;
+            public HanoiTower(HanoiBlock[] blocks) {
+                this.blocks = blocks;
+                top = blocks.length-1;
+            }
+
+            public HanoiTower(int numblocks) {
+                blocks = new HanoiBlock[numblocks];
+                top = -1;
+            }
+
+            public void moveTopTo(HanoiTower t) {
+                System.out.println(this + " .-> " + top);
+                System.out.println("Shifting: " + blocks[top].getSize() + " to " + t);
+                t.addToTop(blocks[top]);
+                blocks[top] = null;
+                if(-1 < top) top--;
+            }
+
+            protected void addToTop(HanoiBlock b) {
+                System.out.println(this);
+                System.out.println("Adding: " + b.getSize() + " to " + this);
+                blocks[++top] = b;
+            }
+
+            public void print() {
+                String out = "";
+                for(HanoiBlock b : blocks)
+                    out += (b!=null? b.getSize() : 0);
+                System.out.println(out);
+            }
         }
     }
 }
