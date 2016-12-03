97efff0cbf8ab89519b4f3e77f334d1a6701fef6
shiro-nk <hiroshikerr@gmail.com>
2016-12-03 11:04:30 -0500

    Hanoi OnClick

--- LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/HanoiActivity.java
+++ LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/HanoiActivity.java
@@ -3,6 +3,7 @@ package com.example.android.limestonehackapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
+import android.view.View;
 
 public class HanoiActivity extends AppCompatActivity {
 
@@ -15,6 +16,7 @@ public class HanoiActivity extends AppCompatActivity {
 /**************************************************************************************************/
     public class Hanoi {
         private HanoiTower[] towers = new HanoiTower[3];
+        private boolean selecting_src = true;
         private int sel_src, sel_tgt, sel_aux;
 
         public Hanoi(int discs) {
@@ -22,9 +24,9 @@ public class HanoiActivity extends AppCompatActivity {
             for(int i = 0; i<discs; i++)
                 blocks[i] = new HanoiBlock(i+1);
 
-            towers[0] = new HanoiTower(blocks, (LinearLayout) findViewById(R.id.ht_src));
-            towers[1] = new HanoiTower(discs,  (LinearLayout) findViewById(R.id.ht_aux));
-            towers[2] = new HanoiTower(discs,  (LinearLayout) findViewById(R.id.ht_tgt));
+            towers[0] = new HanoiTower(blocks, this, 0, (LinearLayout) findViewById(R.id.ht_src));
+            towers[1] = new HanoiTower(discs,  this, 1, (LinearLayout) findViewById(R.id.ht_aux));
+            towers[2] = new HanoiTower(discs,  this, 2, (LinearLayout) findViewById(R.id.ht_tgt));
         }
 
         public void solve(int discs, HanoiTower src, HanoiTower tgt, HanoiTower aux) {
@@ -41,6 +43,18 @@ public class HanoiActivity extends AppCompatActivity {
                 return towers[i];
             else
                 return null;
+        }
+
+        public void selTower(int i) {
+            if(selecting_src) {
+                sel_src = i;
+                selecting_src = false;
+            }
+            else {
+                sel_tgt = i;
+                towers[sel_src].moveTopTo(towers[sel_tgt]);
+                selecting_src = true;
+            }
         }
 
 /**************************************************************************************************/
@@ -60,20 +74,29 @@ public class HanoiActivity extends AppCompatActivity {
 /**************************************************************************************************/
         protected class HanoiTower {
 
+            private Hanoi hanoi;
             // Higher index is top
             private HanoiBlock[] blocks;
             private int top = 0;
+            private int id = 0;
 
             private LinearLayout layout;
 
-            public HanoiTower(HanoiBlock[] blocks, LinearLayout lt) {
+            private HanoiTower(Hanoi h, int id, LinearLayout lt) {
+                this.hanoi = h;
+                this.id = id;
                 this.layout = lt;
+                this.layout.setOnClickListener(onclick);
+            }
+
+            public HanoiTower(HanoiBlock[] blocks, Hanoi h, int id, LinearLayout lt) {
+                this(h, id, lt);
                 this.blocks = blocks;
                 this.top = blocks.length-1;
             }
 
-            public HanoiTower(int numblocks, LinearLayout lt) {
-                this.layout = lt;
+            public HanoiTower(int numblocks, Hanoi h, int id, LinearLayout lt) {
+                this(h, id, lt);
                 this.blocks = new HanoiBlock[numblocks];
                 this.top = -1;
             }
@@ -93,6 +116,12 @@ public class HanoiActivity extends AppCompatActivity {
                 for(HanoiBlock b : blocks)
                     out += (b!=null? b.getSize() : 0);
             }
+
+            private View.OnClickListener onclick = new View.OnClickListener() {
+                public void onClick(View view) {
+                    hanoi.selTower(id);
+                }
+            };
         }
     }
 }
