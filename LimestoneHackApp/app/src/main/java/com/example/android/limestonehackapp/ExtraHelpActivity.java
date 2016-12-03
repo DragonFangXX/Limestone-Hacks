e71fda65fda84a305126cd0e6f4252d477c658f2
EdwinXu <edwin.xu0@gmail.com>
2016-12-03 18:00:00 -0500

    because cyrus said so

--- LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/ExtraHelpActivity.java
+++ LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/ExtraHelpActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.limestonehackapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class ExtraHelpActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class ExtraHelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extra_help);
+    }
+
+    public void openWebPage(String url){
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW,webpage);
+        if(intent.resolveActivity(getPackageManager())!=null)
+        {
+            startActivity(intent);
+        }
     }
 }
