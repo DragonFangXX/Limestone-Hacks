d77c05e90979329fe956fbbaf715377a4741ce2e
Cyrus Ng <cyrus.ng59@gmail.com>
2016-12-03 12:43:06 -0500

    Binary works

--- LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/BinarySearchActivity.java
+++ LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/BinarySearchActivity.java
@@ -2,40 +2,42 @@ package com.example.android.limestonehackapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.EditText;
 
 public class BinarySearchActivity extends AppCompatActivity {
+    public int n = (int) (Math.round(((Math.random()) * 1000))); //Generates random number between 1-1000
+    public int num_guesses = 0; //Counter for # of tries
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_binary_search);
+
     }
 
-    public void compare_loop() {
-        TextView string_progress_message = (TextView)findViewById(R.id.string_progress_message);
-        EditText num_user_input = (EditText)findViewById(R.id.num_user_input);
-        int num_guesses =0; //Counter for # of tries
-        int cmp = 2; // sentinel valuet
+    public void compare_loop(View view) {
+        System.out.println("I'm in'");
+        TextView string_progress_message = (TextView) findViewById(R.id.string_progress_message);
+        TextView numGuesses = (TextView) findViewById(R.id.num_guesses);
+        EditText num_user_input = (EditText) findViewById(R.id.num_user_input);
+
+
         int userInput = Integer.parseInt(num_user_input.getText().toString()); // User's initial Guess
-        int n = (int) (Math.round(((Math.random())*1000))); //Generates random number between 1-1000
 
-        while (cmp != 0) {
-            if (userInput > n) {
-
-                cmp = 1; //User input is greater than generated number
-                string_progress_message.setText("Too High!");
-            } else if (userInput < n) {
-                cmp = -1; // User input is smaller than generated number
-                string_progress_message.setText("Too Low!");
-            } else {
-                cmp = 0; //User input is equal to the generated number
-                string_progress_message.setText("Success!");
-            }
-            userInput = Integer.parseInt(num_user_input.getText().toString());
-            System.out.println(num_guesses);
-            num_guesses++;
+        System.out.println("in");
+        if (userInput > n) {
+            string_progress_message.setText("Too High!");
+        } else if (userInput < n) {
+            string_progress_message.setText("Too Low!");
+        } else {
+            string_progress_message.setText("Success!");
         }
+        System.out.println("out");
+        num_guesses++;
+        System.out.println("im out");
+        numGuesses.setText(Integer.toString(num_guesses));
+        System.out.println("end");
     }
 }
