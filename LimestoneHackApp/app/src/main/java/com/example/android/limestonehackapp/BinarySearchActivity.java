f963d859ef44cd39b1ef00264b707edf80498efe
Cyrus Ng <cyrus.ng59@gmail.com>
2016-12-03 13:11:20 -0500

    whatever got changed

--- LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/BinarySearchActivity.java
+++ LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/BinarySearchActivity.java
@@ -18,15 +18,15 @@ public class BinarySearchActivity extends AppCompatActivity {
     }
 
     public void compare_loop(View view) {
-        System.out.println("I'm in'");
         TextView string_progress_message = (TextView) findViewById(R.id.string_progress_message);
         TextView numGuesses = (TextView) findViewById(R.id.num_guesses);
+        TextView lastGuess = (TextView) findViewById(R.id.last_guess);
+
         EditText num_user_input = (EditText) findViewById(R.id.num_user_input);
 
 
         int userInput = Integer.parseInt(num_user_input.getText().toString()); // User's initial Guess
 
-        System.out.println("in");
         if (userInput > n) {
             string_progress_message.setText("Too High!");
         } else if (userInput < n) {
@@ -34,10 +34,11 @@ public class BinarySearchActivity extends AppCompatActivity {
         } else {
             string_progress_message.setText("Success!");
         }
-        System.out.println("out");
+
         num_guesses++;
-        System.out.println("im out");
-        numGuesses.setText(Integer.toString(num_guesses));
-        System.out.println("end");
+
+        lastGuess.setText();
+        numGuesses.setText(getString(R.string.number_of_guesses, Integer.toString(num_guesses)));
+        num_user_input.setText("");
     }
 }
