03d3ba4f4aae00b97a31694ab714d067928fd897
Cyrus Ng <cyrus.ng59@gmail.com>
2016-12-03 18:36:17 -0500

    progress on extra help url

--- LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/ExtraHelpActivity.java
+++ LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/ExtraHelpActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 
 public class ExtraHelpActivity extends AppCompatActivity {
@@ -14,12 +17,16 @@ public class ExtraHelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_extra_help);
     }
 
-    public void openWebPage(String url){
+    public void getURL(View view){
+        TextView url = (TextView) findViewById(view.getId());
+        openWebPage(url.getText().toString());
+    }
+
+    public void openWebPage(String url) {
         Uri webpage = Uri.parse(url);
-        Intent intent = new Intent(Intent.ACTION_VIEW,webpage);
-        if(intent.resolveActivity(getPackageManager())!=null)
-        {
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
-        }
+           }
     }
 }
