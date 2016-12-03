53e5b188818cd4949844b59175545fc8a310e934
Cyrus Ng <cyrus.ng59@gmail.com>
2016-12-03 14:20:24 -0500

    fixed formatting

--- LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/MainActivity.java
+++ LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showBinaryDrop(View view){
+        ImageView binaryImg = (ImageView)findViewById(R.id.binaryImg);
+        
     }
 
     public void openBinarySearch(View view){
