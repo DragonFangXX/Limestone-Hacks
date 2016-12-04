80abf6954a3b91b6052a8d2f08dcea79c8d5d9bf
EdwinXu <edwin.xu0@gmail.com>
2016-12-03 20:02:34 -0500

    top of tier list

--- LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/LinkedListActivity.java
+++ LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/LinkedListActivity.java
@@ -5,14 +5,14 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class LinkedListActivity extends AppCompatActivity {
-    int counter=0;
+    //int counter=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linked_list);
     }
-    ImageView kirbyImg = (ImageView)findViewById(R.id.kirbyImg1);
-    ImageView ganonImg = (ImageView)findViewById(R.id.ganonImg1);
-    ImageView foxImg = (ImageView)findViewById(R.id.foxImg1);
-    
+    //ImageView kirbyImg = (ImageView)findViewById(R.id.kirbyImg1);
+    //ImageView ganonImg = (ImageView)findViewById(R.id.ganonImg1);
+    //ImageView foxImg = (ImageView)findViewById(R.id.foxImg1);
+
 }
