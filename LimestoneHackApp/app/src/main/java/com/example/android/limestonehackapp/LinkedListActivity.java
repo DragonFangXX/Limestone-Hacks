dcfa17f83afc4124d33ca355a0334ce7b18dcf32
EdwinXu <edwin.xu0@gmail.com>
2016-12-03 19:57:54 -0500

    ganon

--- LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/LinkedListActivity.java
+++ LimestoneHackApp/app/src/main/java/com/example/android/limestonehackapp/LinkedListActivity.java
@@ -2,12 +2,17 @@ package com.example.android.limestonehackapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class LinkedListActivity extends AppCompatActivity {
-
+    int counter=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linked_list);
     }
+    ImageView kirbyImg = (ImageView)findViewById(R.id.kirbyImg1);
+    ImageView ganonImg = (ImageView)findViewById(R.id.ganonImg1);
+    ImageView foxImg = (ImageView)findViewById(R.id.foxImg1);
+    
 }
